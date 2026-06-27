d3626a91c65a771ebe086b6a70a1282058aec058
ezraaslan <ezraaslan10@gmail.com>
2026-06-27 16:55:55 -0400

    codify markers by color + add legend

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 const map = L.map("map").setView([39.9526, -75.1652], 12);
+const markerLayer = L.layerGroup().addTo(map);
 
 L.tileLayer(
 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -41,24 +42,53 @@ map.setView([lat,lon],14);
 });
 
 }
-
 fetch("/api/resources")
     .then(response => response.json())
     .then(resources => {
+
+        markerLayer.clearLayers();
 
         resources.forEach(resource => {
 
             if (!resource.latitude || !resource.longitude)
                 return;
 
-            L.marker([resource.latitude, resource.longitude])
-                .addTo(map)
-                .bindPopup(`
-                    <b>${resource.name || "Unnamed"}</b><br>
-                    <b>Category:</b> ${resource.category}<br>
-                    ${resource.address || ""}<br>
-                    ${resource.phone || ""}
-                `);
+            let color = "gray";
+
+            switch (resource.category) {
+
+                case "Food Bank":
+                    color = "green";
+                    break;
+
+                case "Soup Kitchen":
+                    color = "orange";
+                    break;
+
+                case "Shelter":
+                    color = "blue";
+                    break;
+
+                default:
+                    color = "gray";
+            }
+
+            L.circleMarker(
+                [resource.latitude, resource.longitude],
+                {
+                    radius: 7,
+                    color: color,
+                    fillColor: color,
+                    fillOpacity: 0.85
+                }
+            )
+            .addTo(markerLayer)
+            .bindPopup(`
+                <b>${resource.name || "Unnamed"}</b><br>
+                <b>${resource.category}</b><br>
+                ${resource.address || ""}<br>
+                ${resource.phone || ""}
+            `);
 
         });
 
