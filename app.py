ddd0f3a066434d577de0aec9f430059b8ff37864
ezraaslan <ezraaslan10@gmail.com>
2026-06-27 13:02:11 -0400

    create json

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from database import get_all_shelters
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/api/resources")
+def resources():
+    return jsonify(get_all_shelters())
 
 if __name__ == "__main__":
     app.run(debug=True)
